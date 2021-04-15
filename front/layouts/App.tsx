3383855ea4b979cbe248a07212b49887226810b7
AstronautSion <astronaut.sion@gmail.com>
2021-04-15 17:38:05 +0900

    setting init

---
+++ front/layouts/App.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import LogIn from '@pages/Login';
+import SignUp from '@pages/SignUp';
+
+const App: FC = () => {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
+};
+
+export default App;
